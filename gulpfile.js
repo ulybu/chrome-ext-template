3d63821c482ed761d8403d3601e6c836ed72d976
Ulysse Prygiel <ulysse@hp.com>
2015-10-05 14:33:17 +0200

    add creation of zip

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp')
   , fs = require('fs')
   , sync = require('gulp-config-sync')
+  , zip = require('gulp-zip')
+  , del = require('del')
+  , manifest = require('./manifest.json')
   ;
 var projectProperties = {
-  name:'chrome-ext-template'
+  name:'chrome-ext-template',
+  version: manifest.version
 };
-var zipName = 'rally-ext.zip';
+var zipName = projectProperties.name + '_' + projectProperties.version + '.zip';
 var syncOptions = {
   src: 'manifest.json',
   fields: [
@@ -29,8 +33,22 @@ gulp.task('sync-package-files',function(){
   if(projectProperties.hasBower) {
     filesToSync.push('bower.json');
   }
-  console.log(filesToSync);
+  console.log('Syncing:', filesToSync.join(', '));
   return gulp.src('package.json')
     .pipe(sync(syncOptions))
     .pipe(gulp.dest('.'));
 });
+gulp.task('clean', function(cb) {
+  var zipFile = projectProperties.name + '*.zip';
+  return del(zipFile).then(function(paths){
+    console.log('Deleted files/folders:\n', paths.join('\n\t'));
+  });
+})
+gulp.task('zip' ,['clean'],function() {
+  var include = ['./**']
+    , exclude = ['!node_modules{,/**}','!.git{,/**}','!npm-debug.log']
+    ;
+    return gulp.src(include.concat(exclude))
+    .pipe(zip(zipName))
+    .pipe(gulp.dest('.'));
+})
